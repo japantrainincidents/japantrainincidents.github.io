37e325707958da596789ccce6cdc61210ce9959b
japantrainincidents <64720166+japantrainincidents@users.noreply.github.com>
2020-05-04 21:55:37 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1144,7 +1144,7 @@ function creategeoviz(){
 
     })
 
-    d3.csv("the_real_true_final_version.csv").then(function(injurysites){
+    d3.csv("pre_processing_scripts/the_real_true_final_version.csv").then(function(injurysites){
 
         var projection = d3.geoMercator()
             .center([139.6503,35.6762])
